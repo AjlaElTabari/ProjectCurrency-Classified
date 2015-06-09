2f7e21709ae1e3a6bfd22d345b98fb2be6ce4739
Adis Cehajic <adis.cehajic@BitCamps-Mac-mini-100.local>
2015-06-09 11:55:27 +0200

    Method getExchangeRates

--- src/ba/BITCamp/A_Team/CurrencyProject/Adis.java
+++ src/ba/BITCamp/A_Team/CurrencyProject/Adis.java
@@ -1,10 +1,35 @@
 package ba.BITCamp.A_Team.CurrencyProject;
 
+import lectures.TextIO;
+
 public class Adis {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * Printing exchange rate of wanted currency
+	 * 
+	 * @param currency - Wanted currency
+	 * @return String value of exchange rate
+	 */
+	public static String getExchangeRates(String currency) {
 
+		// Reading file that contains information about exchange currency rates
+		TextIO.readFile("src/lectures/Currency.txt");
+
+		String exchangeRate = "";
+
+		// Going trough file and searching wanted currency rate
+		while (!TextIO.eof()) {
+			String str = TextIO.getln();
+			if (str.indexOf(currency) >= 0) {
+				String[] string = str.split(" ");
+				exchangeRate = string[1];
+
+			}
+		}
+		
+		TextIO.readStandardInput();
+
+		return exchangeRate;
 	}
-
+	
 }
