8795c292f5618b01df1bddee1cb7822780da0fb5
Dinko Hodzic <dinko.hodzic@BitCamps-Mac-mini-54.local>
2015-06-09 11:57:12 +0200

    Dinko

--- src/ba/BITCamp/A_Team/CurrencyProject/Dinko.java
+++ src/ba/BITCamp/A_Team/CurrencyProject/Dinko.java
@@ -2,9 +2,18 @@ package ba.BITCamp.A_Team.CurrencyProject;
 
 public class Dinko {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * <h1>Calculates the conversion</h1>
+	 * The method takes 2 double variables and divides them.
+	 * Then multiplies the result with the wanted amount and gives the result.
+	 * @param currentCurrency : The currency that we want to covert
+	 * @param amount : The wanted amount
+	 * @param desiredCurrency : The currency we want to get
+	 * @return  The result as a double
+	 */
+	public static double calculateConversion(double currentCurrency,
+			double amount, double desiredCurrency) {
+		double result = 0.0;
+		return result = desiredCurrency / currentCurrency * amount;
 	}
-
 }
