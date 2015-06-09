9a45f9cb0d000b3561604fee50c853370d8055b0
Ajla El Tabari <ajla.eltabari@BitCamps-Mac-mini-65.local>
2015-06-09 11:36:27 +0200

    Added TextIO.java

--- src/ba/BITCamp/A_Team/CurrencyProject/Ajla.java
+++ src/ba/BITCamp/A_Team/CurrencyProject/Ajla.java
@@ -1,5 +1,7 @@
 package ba.BITCamp.A_Team.CurrencyProject;
 
+import ba.BITCamp.ajla.s04d02.predavanja.TextIO;
+
 public class Ajla {
 
 	public static void main(String[] args) {
@@ -7,4 +9,12 @@ public class Ajla {
 
 	}
 
+	public static void inputValues(String[] args) {
+		String currentCurrency = args[0];
+		Double amount = Double.parseDouble(args[1]);
+		String desiredCurrency = args[1];
+		
+		TextIO.readFile("src/ba/BITCamp/ajla/s04d02/predavanja/Currency.txt");
+		String a = TextIO.getlnString();
+	}
 }
